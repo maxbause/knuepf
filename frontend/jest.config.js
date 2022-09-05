f9d406051c586103fe5ae04bc2598b8fbab0b60d
Max Bause <max.w.bause@gmail.com>
2022-09-05 23:16:11 +0200

    feat: improves fe testing infrastructure

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testMatch: ['**/__specs__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  automock: false,
+  resetMocks: false,
+  setupFiles: [
+    './specs/unit/spec-initializer.ts',
+  ],
 }
